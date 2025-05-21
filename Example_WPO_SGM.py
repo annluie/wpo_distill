de7e4de9cc51881a1c50d7261ec10b3c125b54c2
Annie <annluie2000@gmail.com>
2025-05-20 20:14:20 -0700

    syncing the ipynb and python files

--- Example_WPO_SGM.py
+++ Example_WPO_SGM.py
@@ -16,9 +16,10 @@ def install_dependencies():
         print(f"Error running installation script: {e}")
 import matplotlib.pyplot as plt
 import os
+import sys
+sys.path.append('..')
 
 import torch
-#torch.cuda.empty_cache()
 import torch.optim as optim
 import torch.nn as nn
 from torch.distributions.multivariate_normal import MultivariateNormal
@@ -95,18 +96,18 @@ if __name__ == "__main__":
     # In[3]:
     device=torch.device('cpu')
 
-    parser = argparse.ArgumentParser(' ')
-    parser.add_argument('--data', choices=['swissroll', '8gaussians', 'pinwheel', 'circles', 'moons', '2spirals', 'checkerboard', 'rings','swissroll_6D_xy1', 'cifar10'], type = str,default = '2spirals')
-    parser.add_argument('--depth',help = 'number of hidden layers of score network',type =int, default = 5)
-    parser.add_argument('--hiddenunits',help = 'number of nodes per hidden layer', type = int, default = 64)
-    parser.add_argument('--niters',type = int, default = 500)
-    parser.add_argument('--batch_size', type = int,default = 20)
-    parser.add_argument('--lr',type = float, default = 2e-3) 
-    parser.add_argument('--save',type = str,default = 'experiments/')
-    parser.add_argument('--train_kernel_size',type = int, default = 100)
-    parser.add_argument('--train_samples_size',type = int, default = 500)
-    parser.add_argument('--test_samples_size',type = int, default = 50)
-    args = parser.parse_args('')
+parser = argparse.ArgumentParser(' ')
+parser.add_argument('--data', choices=['swissroll', '8gaussians', 'pinwheel', 'circles', 'moons', '2spirals', 'checkerboard', 'rings','swissroll_6D_xy1', 'cifar10'], type = str,default = '2spirals')
+parser.add_argument('--depth',help = 'number of hidden layers of score network',type =int, default = 5)
+parser.add_argument('--hiddenunits',help = 'number of nodes per hidden layer', type = int, default = 64)
+parser.add_argument('--niters',type = int, default = 50)
+parser.add_argument('--batch_size', type = int,default = 4)
+parser.add_argument('--lr',type = float, default = 2e-3) 
+parser.add_argument('--save',type = str,default = 'cifar10_experiments/')
+parser.add_argument('--train_kernel_size',type = int, default = 10)
+parser.add_argument('--train_samples_size',type = int, default = 500)
+parser.add_argument('--test_samples_size',type = int, default = 5)
+args = parser.parse_args('')
 
 
     # In[4]:
@@ -152,75 +153,82 @@ if __name__ == "__main__":
 
     # In[6]:
 
-    @profile
-    def evaluate_model(factornet, kernel_centers, num_test_sample):
-        '''
-        Evaluate the model by computing the average total loss over 10 batch of testing samples
-        '''
-        total_loss_sum = 0
-        device = kernel_centers.device
-        for i in range(10):
-            p_samples = toy_data.inf_train_gen(dataset,batch_size = num_test_sample)
-            testing_samples = torch.tensor(p_samples).to(dtype = torch.float32).to(device)
-            total_loss = LearnCholesky.score_implicit_matching(factornet,testing_samples,kernel_centers)
-            total_loss_sum += total_loss.item()
-            # free memory
-            del p_samples,testing_samples,total_loss
-            torch.cuda.empty_cache()
-        average_total_loss = total_loss_sum / 10
-        return average_total_loss
+@profile
+def evaluate_model(factornet, kernel_centers, num_test_sample):
+    '''
+    Evaluate the model by computing the average total loss over 10 batch of testing samples
+    '''
+    total_loss_sum = 0
+    device = kernel_centers.device
+    for i in range(10):
+        p_samples = toy_data.inf_train_gen(dataset,batch_size = num_test_sample)
+        testing_samples = torch.tensor(p_samples).to(dtype = torch.float32).to(device)
+        total_loss = LearnCholesky.score_implicit_matching(factornet,testing_samples,kernel_centers)
+        total_loss_sum += total_loss.item()
+         # Free up memory
+        del p_samples, testing_samples, total_loss
+        #gc.collect() #only if using CPU
+        torch.cuda.empty_cache()  # Only if using GPU
+    average_total_loss = total_loss_sum / 10
+    return average_total_loss
 
-    @profile
-    def save_training_slice_cov(factornet, means, epoch, lr, batch_size, loss_value, save):
-        '''
-        Save the training slice of the density plot
-        '''
-        if means.shape[1] != 2:
-            return
-        plot_axis = means.max().item() * 1.1
-        device = means.device
-        # Create x as a NumPy array
-        x_np = np.meshgrid(np.linspace(-plot_axis, plot_axis, 200), np.linspace(-plot_axis, plot_axis, 200))
-        x_np = np.stack(x_np, axis=-1).reshape(-1, 2)
+def save_training_slice_cov(factornet, means, epoch, lr, batch_size, loss_value, save):
+    '''
+    Save the training slice of the NN
+    '''
+    if save is not None:
+        torch.save(factornet.state_dict(), save + 'epoch' + str(epoch) + 'model_weights.pth')
 
-        x = torch.tensor(x_np, dtype=torch.float32, device=device)
-        data_dim = x.shape[1]
-        precisions = LearnCholesky.vectors_to_precision(factornet(means),data_dim)
-        density = LearnCholesky.mog_density(x, means, precisions)
-        density = density.reshape(200, 200).T
+    """ OLD CODE
+    if means.shape[1] != 2:
+        return
+    plot_axis = means.max().item() * 1.1
+    device = means.device
+    # Create x as a NumPy array
+    x_np = np.meshgrid(np.linspace(-plot_axis, plot_axis, 200), np.linspace(-plot_axis, plot_axis, 200))
+    x_np = np.stack(x_np, axis=-1).reshape(-1, 2)
 
-        # Create a figure
-        fig = plt.figure(figsize=(8, 4))
-        plt.clf()
-        
-        plt.subplot(1, 2, 1) 
-        plt.contourf(np.linspace(-plot_axis, plot_axis, 200), np.linspace(-plot_axis, plot_axis, 200), density.detach().cpu().numpy(), cmap='viridis')
-        plt.axis('square')
-        plt.colorbar()     
-        
-        plt.subplot(1, 2, 2) 
-        plt.contourf(np.linspace(-plot_axis, plot_axis, 200), np.linspace(-plot_axis, plot_axis, 200), density.detach().cpu().numpy(), cmap='viridis')
-        # Plot the centers
-        num_components = torch.min(torch.tensor([means.shape[0], 400]))
-        plot_centers = means[:num_components].detach().cpu().numpy()
-        plt.scatter(plot_centers[:,1], plot_centers[:,0], s=0.2, c='r')
-        plt.axis('square')
-        # plt.colorbar()    
-        plt.title(f'Epoch: {epoch}, Loss: {loss_value:.3e}')
-                
-        plt.tight_layout()  # Improve subplot spacing
+    x = torch.tensor(x_np, dtype=torch.float32, device=device)
+    data_dim = x.shape[1]
+    precisions = LearnCholesky.vectors_to_precision(factornet(means),data_dim)
+    density = LearnCholesky.mog_density(x, means, precisions)
 
-        # Save the figure
-        lr_str = f'{lr:.2e}'
-        if save is not None:
-            plt.savefig(f'{save}batch_size_{batch_size}lr_{lr_str}_epoch_{epoch}.png')
+    density = density.reshape(200, 200).T
 
-        plt.close(fig)
+    # Create a figure
+    fig = plt.figure(figsize=(8, 4))
+    plt.clf()
+    
+    plt.subplot(1, 2, 1) 
+    plt.contourf(np.linspace(-plot_axis, plot_axis, 200), np.linspace(-plot_axis, plot_axis, 200), density.detach().cpu().numpy(), cmap='viridis')
+    plt.axis('square')
+    plt.colorbar()     
+    
+    plt.subplot(1, 2, 2) 
+    plt.contourf(np.linspace(-plot_axis, plot_axis, 200), np.linspace(-plot_axis, plot_axis, 200), density.detach().cpu().numpy(), cmap='viridis')
+    # Plot the centers
+    num_components = torch.min(torch.tensor([means.shape[0], 400]))
+    plot_centers = means[:num_components].detach().cpu().numpy()
+    plt.scatter(plot_centers[:,1], plot_centers[:,0], s=0.2, c='r')
+    plt.axis('square')
+    # plt.colorbar()    
+    plt.title(f'Epoch: {epoch}, Loss: {loss_value:.3e}')
+             
+    plt.tight_layout()  # Improve subplot spacing
+
+    # Save the figure
+    lr_str = f'{lr:.2e}'
+    if save is not None:
+        plt.savefig(f'{save}batch_size_{batch_size}lr_{lr_str}_epoch_{epoch}.png')
+
+    plt.close(fig)
+    """
+    
 
 
     # ### Initialize score network
 
-    # In[7]:
+# In[8]:
 
 
     # check the dataset
@@ -238,7 +246,7 @@ if __name__ == "__main__":
     # ## Initialize Data using CIFAR-10
     # 
 
-    # In[8]:
+# In[9]:
 
 
     # check the dataset
@@ -266,72 +274,136 @@ if __name__ == "__main__":
     factornet = construct_factor_model(data_dim, depth, hidden_units).to(device).to(dtype = torch.float32)
     #factornet = torch.compile(factornet) # help speed up training
 
-    lr = args.lr
-    optimizer = optim.Adam(factornet.parameters(), lr=args.lr)
+lr = args.lr
+optimizer = optim.Adam(factornet.parameters(), lr=args.lr)
 
-    p_samples = toy_data.inf_train_gen(dataset,batch_size = train_samples_size)
-    training_samples = torch.tensor(p_samples).to(dtype = torch.float32).to(device)
-    centers  = torch.tensor(toy_data.inf_train_gen(dataset, batch_size = train_kernel_size)).to(dtype = torch.float32).to(device)
+p_samples = toy_data.inf_train_gen(dataset,batch_size = train_samples_size)
+training_samples = torch.tensor(p_samples).to(dtype = torch.float32).to(device)
+centers  = torch.tensor(toy_data.inf_train_gen(dataset, batch_size = train_kernel_size)).to(dtype = torch.float32).to(device)
 
-    # torch.save(centers, save_directory + 'centers.pt')
+torch.save(centers, save_directory + str(epochs) + 'epochs' + str(train_kernel_size) + 'centers.pt')
 
-    epochs = args.niters
-    batch_size = args.batch_size
+epochs = args.niters
+batch_size = args.batch_size
 
-    # Training the score network
-    loss = evaluate_model(factornet, centers, test_samples_size)
-    formatted_loss = f'{loss:.3e}'  # Format the average with up to 1e-3 precision
-    print(f'Before train, Average total_loss: {formatted_loss}')
-
-    #print(torch.cuda.memory_summary(device=None, abbreviated=False))
-    # In[ ]:
+# Training the score network
+loss = evaluate_model(factornet, centers, test_samples_size)
+formatted_loss = f'{loss:.3e}'  # Format the average with up to 1e-3 precision
+print(f'Before train, Average total_loss: {formatted_loss}')
 
 
-    for step in range(epochs):
-        # samples_toydata
-        randind = torch.randint(0,train_samples_size,[batch_size,])
-        samples = training_samples[randind,:]
+# In[11]:
 
-        loss = LearnCholesky.score_implicit_matching(factornet,samples,centers)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+
+#@profile
+def opt_check(factornet, samples, centers):
+    loss = LearnCholesky.score_implicit_matching(factornet,samples,centers)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss.item()
+
+# Use trange instead of range to get tqdm progress bar
+for step in trange(epochs, desc="Training"):
+    print(f"Step {step} started")
+    randind = torch.randint(0, train_samples_size, [batch_size,])
+    samples = training_samples[randind, :]
+    loss_value = opt_check(factornet, samples, centers)
+
+    if step % 4000 == 0:
+        print(f'Step: {step}, Loss value: {loss_value:.3e}')
+    
+    if step % 20000 == 0:
+        loss0 = evaluate_model(factornet, centers, test_samples_size)
+        save_training_slice_cov(factornet, centers, step, lr, batch_size, loss0, save_directory)
         
-        if not step % 4000:
-            loss_value = loss.item()
-            print(f'Step: {step}, Loss value: {loss_value:.3e}')
+    if step < epochs - 1:
+        del samples
+        gc.collect()
+        torch.cuda.empty_cache()
 
-        if not step % 20000:
-            loss0 = evaluate_model(factornet, centers, test_samples_size)
-            save_training_slice_cov(factornet, centers, step, lr, batch_size, loss0, save_directory)
 
-    formatted_loss = f'{loss:.3e}'  # Format the average with up to 1e-3 precision
-    print(f'After train, Average total_loss: {formatted_loss}')
+# In[13]:
+
+
+loss = evaluate_model(factornet, centers, test_samples_size)    
+save_training_slice_cov(factornet, centers, step, lr, batch_size, loss0, save_directory)
+formatted_loss = f'{loss:.3e}'  # Format the average with up to 1e-3 precision
+print(f'After train, Average total_loss: {formatted_loss}')
+
+
+# In[10]:
+
+
+import time
+
+for step in range(epochs):
+    start = time.time()
+    # samples_toydata
+    randind = torch.randint(0,train_samples_size,[batch_size,])
+    samples = training_samples[randind,:]
+    loss_value = opt_check(factornet, samples, centers)
+    """
+    loss = LearnCholesky.score_implicit_matching(factornet,samples,centers)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    """
+   
+    
+    if not step % 4000:
+        #loss_value = loss.item()
+        print(f'Step: {step}, Loss value: {loss_value:.3e}')
+
+    if not step % 20000:
+        loss0 = evaluate_model(factornet, centers, test_samples_size)
+        save_training_slice_cov(factornet, centers, step, lr, batch_size, loss0, save_directory)
+    if step < epochs - 1:
+         # Free up memory
+        del samples # del loss
+        gc.collect()
+        torch.cuda.empty_cache()
+    print("Elapsed time:", time.time() - start)
+    
+loss = evaluate_model(factornet, centers, test_samples_size)    
+save_training_slice_cov(factornet, centers, step, lr, batch_size, loss0, save_directory)
+formatted_loss = f'{loss:.3e}'  # Format the average with up to 1e-3 precision
+print(f'After train, Average total_loss: {formatted_loss}')
 
     print(torch.cuda.memory_summary(device=None, abbreviated=False))
     # In[ ]:
 
 
-    # sample from trained model
-    # and plot density
-    precisions = LearnCholesky.vectors_to_precision(factornet(centers),data_dim)
-    LearnCholesky.plot_images(centers, precisions,plot_number=10,save_path=save_directory + 'samples.png')
+# sample from trained model
+# and plot density
+#file /wpo_distill/cifar10_experiments/test/epoch499model_weights.pth
+#factornet.load_state_dict(torch.load('wpo_distill/cifar10_experiments/test/epoch499model_weights.pth'))
+print(os.path.exists("/home/jupyter/wpo_distill/cifar10_experiments/test/epoch499model_weights.pth"))
+print("Current working dir:", os.getcwd())
+model = construct_factor_model(data_dim, depth, hidden_units).to(device).to(dtype = torch.float32)
+model.load_state_dict(torch.load("/home/jupyter/wpo_distill/cifar10_experiments/test/epoch499model_weights.pth"))
 
-    """ I think this will not work for general (centers needs to be same as before to plot properly)
-    randind = torch.randint(0,1000,[1000,])
-    centers = means[randind,:].to(device)
-    precisions = LearnCholesky.vectors_to_precision(factornet(centers),data_dim)
+with torch.no_grad():
+        
+    precisions = LearnCholesky.vectors_to_precision(model(centers),data_dim)
 
-    LearnCholesky.scatter_samples_from_model(centers, precisions, dim1 = 0, dim2 = 1,save_path=save_directory + 'samples.png')
-    LearnCholesky.plot_density_2d_marg(centers,factornet,dim1 = 0, dim2 = 1, save_path=save_directory + 'density.png')
-    """
+    LearnCholesky.plot_images(centers, precisions, epoch = 49, plot_number=10, save_path=save_directory + 'samples')
+
+""" I think this will not work for general (centers needs to be same as before to plot properly)
+randind = torch.randint(0,1000,[1000,])
+centers = means[randind,:].to(device)
+precisions = LearnCholesky.vectors_to_precision(factornet(centers),data_dim)
+
+LearnCholesky.scatter_samples_from_model(centers, precisions, dim1 = 0, dim2 = 1,save_path=save_directory + 'samples.png')
+LearnCholesky.plot_density_2d_marg(centers,factornet,dim1 = 0, dim2 = 1, save_path=save_directory + 'density.png')
+"""
 
 
-    # In[ ]:
+# In[ ]:
 
 
-    # LearnCholesky.scatter_samples_from_model(centers, precisions, dim1 = 2, dim2 = 3, save_path=save_directory + 'samples.png')
-    # LearnCholesky.plot_density_2d_marg(centers,factornet, dim1 = 2, dim2 = 3, save_path=save_directory + 'density.png')
-    # LearnCholesky.scatter_samples_from_model(centers, precisions, dim1 = 4, dim2 = 5,  save_path=save_directory + 'samples.png')
-    # LearnCholesky.plot_density_2d_marg(centers,factornet, dim1 = 4, dim2 = 5,save_path=save_directory + 'density.png')
+# LearnCholesky.scatter_samples_from_model(centers, precisions, dim1 = 2, dim2 = 3, save_path=save_directory + 'samples.png')
+# LearnCholesky.plot_density_2d_marg(centers,factornet, dim1 = 2, dim2 = 3, save_path=save_directory + 'density.png')
+# LearnCholesky.scatter_samples_from_model(centers, precisions, dim1 = 4, dim2 = 5,  save_path=save_directory + 'samples.png')
+# LearnCholesky.plot_density_2d_marg(centers,factornet, dim1 = 4, dim2 = 5,save_path=save_directory + 'density.png')
 
